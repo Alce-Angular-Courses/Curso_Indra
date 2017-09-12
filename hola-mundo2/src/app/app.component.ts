34ec7ef8ba48937fac5ead0691a3c04d1989d5ee
alce65 <alce65@hotmail,es>
2017-09-12 12:21:26 +0200

    Nuevas modificaciones en Hola Mundo

--- hola-mundo2/src/app/app.component.ts
+++ hola-mundo2/src/app/app.component.ts
@@ -5,8 +5,9 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-
 export class AppComponent {
   title = 'Angular 2';
   empresa = 'Indra'
 }
+
+
